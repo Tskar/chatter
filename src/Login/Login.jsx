9fe7424e0f7dc7cfc97aa5be2121bebadba4e857
Tskar <sanskarthapaofficial@gmail.com>
2024-10-14 02:33:57 -0400

    Login Page Implementation issue id #4

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,13 +1,39 @@
-import React from "react"
+import React, { useState } from "react"
 import './login.css'
+import Applogo from '../Images/App-logo.png'
+
+import app from "../Firebase/firebase";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+
 
 const Login = (props) => {
+
+    const [user, setUser] = useState(null);
+    const auth = getAuth(app);
+    const googleLogin = () => {
+        const provider = new GoogleAuthProvider();
+        provider.addScope("https://www.googleapis.com/auth/userinfo.profile");
+
+        signInWithPopup(auth,provider).then((result)=> {
+            setUser(result.user);            
+        });
+    }
+
+    const signOut = async () => {
+        await auth.signOut();
+        setUser(null);
+    }
+
   return (
     <div className="login">
-      <img src="" alt=""></img>
+      <img src={Applogo} alt=""></img>
       <div className="button-div">
-        <button className="sign-in">Sign in with Google</button>
+        {!user && <button className="sign-in" onClick={googleLogin}>Sign in with Google</button>}
+
+        {/*Testing signOut and user persistence. Will be moved to app canvas. */}
+        {user && <button className="sign-in" onClick={signOut}>Sign Out</button>}
       </div>
+      
     </div>
   )
 };
