2955e23352ce6b9dbf1c2dea143a06ffaef6fa89
Tskar <sanskarthapaofficial@gmail.com>
2024-10-14 15:22:10 -0400

    [NEW FEATURE] - UI - Application theme selection #3

---
+++ src/Context/ThemeContext.js
@@ -0,0 +1,24 @@
+import React, { useState, createContext, useContext, useEffect } from 'react';
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+//To control theme through the application (toggle between light and dark)
+export const useTheme = () => useContext(ThemeContext);
