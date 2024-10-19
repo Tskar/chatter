2d7bf7acd73b40be1fa8a33753ba02a9229d4453
Tskar <sanskarthapaofficial@gmail.com>
2024-10-18 20:52:42 -0400

    [BUG] - App.js - Application glitches when launched. #20

--- src/App.js
+++ src/App.js
@@ -2,15 +2,38 @@ import './App.css';
 import Login from './Login/Login';
 import Canvas from './Components/Canvas/Canvas';
 import { useAuth } from './Context/AuthContext';
+import LoadImg from "./Images/loading-gif.gif"
+
+import { useState, useEffect } from "react";
 
 function App() {
+  const { currentUser } = useAuth();
+  const [loading, setLoading] = useState(true); // loading state to avoid rendering canvas page before login.
 
-  const {currentUser}= useAuth();
+
+  useEffect(() => {
+    const checkAuthStatus = async () => {
+      try {
+
+        await new Promise((resolve) => setTimeout(resolve, 1000));
+        setLoading(false);
+      } catch (error) {
+        console.error("Error checking authentication status", error);
+        setLoading(false);
+      }
+    };
+
+    checkAuthStatus();
+  }, []);
+
+
+  if (loading) {
+    return <img src={LoadImg} alt='Checking authentication status...'></img>;
+  }
 
   return (
     <div className="app">
-      {!currentUser && <Login />}
-      {currentUser && <Canvas />}
+      {currentUser ? <Canvas /> : <Login />}
     </div>
   );
 }
